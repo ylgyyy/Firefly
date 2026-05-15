c5f88b46633cf41739505db58430fe03009009d6
siilao <97461255@qq.com>
2026-05-15 13:55:10 +0800

    第一篇文章

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,13 +6,13 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "https://img.887858.xyz/i/u/2026/05/15/m6hnet.jpg",
 
 	// 名字
-	name: "Firefly",
+	name: "戏子",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "一个爱分享的博主！",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -24,19 +24,19 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "https://qm.qq.com/q/W2LwhPD9ya",
 			showName: false,
 		},
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/siilao",
 			showName: false,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "97461255@qq.com@msn.com",
 			showName: false,
 		},
 		{
