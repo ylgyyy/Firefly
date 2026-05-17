16dc47ce8a4e0f7d9d7b45e891fc17f7ff61b603
siilao <97461255@qq.com>
2026-05-17 13:36:09 +0800

    vault backup: 2026-05-17 13:36:09

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -6,7 +6,7 @@ export const commentConfig: CommentConfig = {
 
 	//twikoo评论系统配置，版本1.7.4
 	twikoo: {
-		envId: "https://twikoo.887858.xyz",
+		envId: "https://twikoo.trrr.top",
 		// 设置 Twikoo 评论系统语言
 		lang: "zh-CN",
 		// 是否启用文章访问量统计功能
